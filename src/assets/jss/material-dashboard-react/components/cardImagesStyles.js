ddc3fd3e6025d6a599cf40b7c015a658e39256ab
johnnyzaet08 <johnny.zaet08@gmail.com>
2022-10-28 17:19:46 -0600

    Plantilla trabajada
    Se implementa una plantilla en la cual se comienzan a realizar los cambios requeridos para sacar los user storys para este sprint. Por el momento se presentan no funcionales.

---
+++ src/assets/jss/material-dashboard-react/components/cardImagesStyles.js
@@ -0,0 +1,18 @@
+const cardImagesStyles = {
+  cardImages: {
+    "&$cardImageProfile img": {
+      width: "100%",
+      height: "auto",
+    },
+  },
+  cardImageProfile: {
+    maxWidth: "150px",
+    maxHeight: "150px",
+    margin: "-15px -30px 0px",
+    borderRadius: "10%",
+    overflow: "hidden",
+    padding: "0%",
+  },
+};
+
+export default cardImagesStyles;
