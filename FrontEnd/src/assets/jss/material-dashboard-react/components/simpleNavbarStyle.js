228647331f4ed9294a42b7042a8fcec675e5d73b
johnnyzaet08 <johnny.zaet08@gmail.com>
2022-11-12 13:36:25 -0600

    Add cacheStorage, modificated viewTournament routes and create simpleNavbar
    Create a new component simpleNavbar from viewtournament view. Add the new file that content the simpleNavbar style's. Create code for use a cacheStorage, delete and modificated a diferentes important files

--- FrontEnd/src/assets/jss/material-dashboard-react/components/simpleNavbarStyle.js
+++ FrontEnd/src/assets/jss/material-dashboard-react/components/simpleNavbarStyle.js
@@ -1,15 +1,57 @@
 import {
     defaultFont,
-    blackColor
+    blackColor,
+    primaryColor,
+    grayColor,
 } from "assets/jss/material-dashboard-react.js";
 
 const simpleNavbarStyle = () => ({
 
-    styleDiv:{
-        height: 10,
-        color: blackColor,
-        font: defaultFont,
+  list: {
+    marginTop: "0px",
+    paddingLeft: "0",
+    paddingTop: "0",
+    paddingBottom: "0",
+    marginBottom: "0",
+    listStyle: "none",
+    position: "unset",
+    display: "flex",
+    flexDirection: "row",
+    
+  },
+  item: {
+    position: "right",
+    display: "block",
+    paddingLeft: "100px",
+    textDecoration: "none",
+    "&:selected":{
+      color: primaryColor[0],
     },
+    "&:hover,&:focus,&:visited,&": {
+      color: primaryColor[0],
+    },
+  },
+  itemLink: {
+    width: "auto",
+    transition: "all 300ms linear",
+    margin: "10px 15px 0",
+    borderRadius: "8%",
+    position: "relative",
+    display: "block",
+    padding: "10px 15px",
+    backgroundColor: grayColor[3],
+    ...defaultFont,
+  },
+  itemText: {
+    ...defaultFont,
+    margin: "0",
+    lineHeight: "20px",
+    fontSize: "15px",
+    color: blackColor,
+    "&:inset":{
+      color: primaryColor[0],
+    },
+  },
 
 });
 
