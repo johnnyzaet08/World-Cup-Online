d383e0a21de2757236c586230adf43a71e30de00
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2022-11-23 23:52:31 -0600

    Descomentando codigo

--- FrontEnd/src/views/Dashboard/Dashboard.js
+++ FrontEnd/src/views/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
-//import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import React from "react";
+//import React from "react";
 
 // react plugin for creating charts
 // @material-ui/core
@@ -15,12 +15,12 @@ import CardImages from "components/Card/CardImages";
 import CardFooter from "components/Card/CardFooter.js";
 import Button from "components/CustomButtons/Button.js";
 import avatar from "assets/img/copa.png";
-//import axios from "axios";
+import axios from "axios";
 
 export default function Dashboard() {
   
   const history = useHistory();
-  /*
+  
   const [
     isLoading, 
     setLoading] = useState(true);
@@ -34,8 +34,8 @@ export default function Dashboard() {
     getTournaments();
     getTournamentsID();
   }, []);
-  */
- /*
+  
+ 
   const getTournaments = () => {
     axios.get('http://localhost:5000/getTournament')
                 .then(response => {
@@ -60,11 +60,11 @@ export default function Dashboard() {
                   console.error('There was an error!', error);
                 });
     }
-  */
-  //const Description = tournaments;
-  //const Index = tournamentsID;
-  const Description = ["ID-1","ID-2","ID-3","ID-4","ID-5"];
-  const Index = ["ID1","ID2","ID3","ID4","ID5"];
+  
+  const Description = tournaments;
+  const Index = tournamentsID;
+  //const Description = ["ID-1","ID-2","ID-3","ID-4","ID-5"];
+  //const Index = ["ID1","ID2","ID3","ID4","ID5"];
 
   const handleInputChange = (i) => {
     sessionStorage.setItem("TournamentID", Index[i]);
@@ -86,11 +86,10 @@ export default function Dashboard() {
   if (sessionStorage.getItem("Verified") !== "true"){
     alert("Error al inicio de sesion");
     history.push("/auth/login-page");
-  }/*
+  }
   else if (isLoading) {
     return <div className="CreateTournament">Loading...</div>;
   }
-  */
   else{
     return (
       <div>
